14d32883a56678a5e83e62c9664c9f55a8fc39f0
Joaquin Leimeter <leimeter.joaquin@gmail.com>
2022-03-03 21:03:59 -0300

    fuck tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        iconikamain: "F4D4E0",
-        "iconika-main-highlight": "F4D4E0",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
